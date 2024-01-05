29222b6065921cc9af4dfef8262ba594f5ed18b1
mirabs <bnsalemamira@gmail.com>
2024-01-05 11:40:19 +0100

    change text2

--- src/App.js
+++ src/App.js
@@ -28,7 +28,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-         HELLO
+          <h1>HELLO</h1>
             {data}
         </a>
       </header>
